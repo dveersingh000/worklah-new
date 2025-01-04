9469fd68f0ca83329132b197317a6944493763c4
Dharamveer Singh <dveersingh000@gmail.com>
2025-01-04 15:05:47 +0530

    new changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {  })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
